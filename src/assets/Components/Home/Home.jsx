25a18ff5976e39889479aa2f0687b153efd0cbbf
monirzkhan <odvutkabbo@gmail.com>
2026-03-28 23:04:48 +0600

    hero image set

--- src/assets/Components/Home/Home.jsx
+++ src/assets/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react'
-import BgImage from '../../../../public/tea-glass-with-dates-fruit-beads-canvas.jpg'
+
+
 
 
 const Home = () => {
@@ -10,24 +11,27 @@ const Home = () => {
     
   }
   return (
-    <div className='h-screen   mx-auto text-center '
+    <div className='hero min-h-screen text-center'
       style={{
     backgroundImage:
-      {BgImage},
+      "url('https://i.ibb.co.com/FLpsDsCn/2554172-13591.jpg')",
+      
   }}
    
   >
 
-      <h1 className='text-2xl mt-5 font-bold font-ubuntu'>Daily Tasbih</h1>
-      <div className="card h-full mx-auto text-center flex justify-center items-center" 
+      <div className='hero-overlay'>
+        <h1 className='text-2xl mt-5 font-bold font-ubuntu'>Daily Tasbih</h1>
+      <div className="hero-content card h-full mx-auto text-center flex justify-center items-center " 
       >
         <button 
         style={{ "--value": count }} aria-valuenow={count} role="progressbar"
-        className='p-8 text-8xl border-4 h-40 w-32 shadow-2xl radial-progress bg-primary text-primary-content border-primary' onClick={() => HandleTasbih()}>
+        className=' p-8 text-8xl border-4 h-40 w-32 shadow-xl radial-progress bg-primary text-primary-content border-primary' onClick={HandleTasbih}>
           {count}
         </button>
        
 
+      </div>
       </div>
 
     </div>
