7f4320d43dcf708449c3a67aac5fc28b32d74ce1
monirzkhan <odvutkabbo@gmail.com>
2026-03-29 23:23:07 +0600

    sound added

--- src/assets/Components/Home/Home.jsx
+++ src/assets/Components/Home/Home.jsx
@@ -1,5 +1,5 @@
-import { HomeIcon, Moon, RotateCcw, Settings, Settings2, Sun, Volume2, VolumeX } from 'lucide-react';
-import React from 'react';
+import { HomeIcon, Moon, RotateCcw, Sun, Volume2, VolumeX } from 'lucide-react';
+import React, { useRef } from 'react';
 import { useState } from 'react'
 
 import Swal from 'sweetalert2';
@@ -11,49 +11,65 @@ const Home = () => {
   const [theme, setTheme] = useState('coffee');
   const [count, setCount] = useState(0)
   const [progress, setProgress] = useState(0);
+  const [soundOn, setSoundOn] = useState(true);
+
+
+  const audioRef = useRef(new Audio('/pop.mp3'))
+
 
   const HandleReset = () => {
-  const swalWithBootstrapButtons = Swal.mixin({
-    customClass: {
-      confirmButton: "btn btn-success btn-lg",
-      cancelButton: "btn btn-danger btn-lg mr-2"
-    },
-    buttonsStyling: false
-  });
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success btn-lg",
+        cancelButton: "btn btn-danger btn-lg mr-2"
+      },
+      buttonsStyling: false
+    });
 
-  swalWithBootstrapButtons.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonText: "Yes, reset it!",
-    cancelButtonText: "No, cancel!",
-    reverseButtons: true
-  }).then((result) => {
+    swalWithBootstrapButtons.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, reset it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true
+    }).then((result) => {
 
-    if (result.isConfirmed) {
-      
-      setCount(0);
-      setProgress(0);
+      if (result.isConfirmed) {
 
-      swalWithBootstrapButtons.fire({
-        title: "Reset!",
-        text: "Your tasbih has been reset.",
-        icon: "success"
-      });
-    } 
-    
-    else if (result.dismiss === Swal.DismissReason.cancel) {
-      swalWithBootstrapButtons.fire({
-        title: "Cancelled",
-        text: "Your Tasbih count is safe :)",
-        icon: "error"
-      });
-    }
-  });
-};
+        setCount(0);
+        setProgress(0);
+
+        swalWithBootstrapButtons.fire({
+          title: "Reset!",
+          text: "Your tasbih has been reset.",
+          icon: "success"
+        });
+      }
+
+      else if (result.dismiss === Swal.DismissReason.cancel) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          text: "Your Tasbih count is safe :)",
+          icon: "error"
+        });
+      }
+    });
+  };
 
   const HandleTasbih = () => {
+
+    if (soundOn) {
+      audioRef.current.currentTime = 0;
+      audioRef.current.volume = 0.3;
+      audioRef.current.play().catch(() => { });
+    }
+
+    if (navigator.vibrate) {
+      navigator.vibrate(15);
+    }
+
     setCount(prev => {
       if (prev === 100) {
         setProgress(p => p + 1);
@@ -88,26 +104,26 @@ const Home = () => {
               : <Moon className='size-16 md:size-20  btn btn-primary p-4 btn-circle'></Moon>
           }
           </button>
-          <button>
+          <button onClick={() => setSoundOn(prev => !prev)}>
             {
-              theme === 'autumn' ? <Volume2 className='size-16 md:size-20   btn btn-primary p-4 btn-circle'/>
-              : <VolumeX className='size-16 md:size-20  btn btn-primary p-4 btn-circle'/>
+              soundOn ? <Volume2 className='size-16 md:size-20   btn btn-primary p-4 btn-circle' />
+                : <VolumeX className='size-16 md:size-20  btn btn-primary p-4 btn-circle' />
             }
           </button>
 
         </div>
-        <div className=" hero-content max-w-md card h-full mx-auto text-center flex justify-start mt-35 items-center "
+        <div className=" hero-content max-w-md card h-full mx-auto text-center flex justify-start mt-55 items-center "
         >
           <button
             style={{ "--value": count, "--thickness": "6px" }}
             aria-valuenow={count}
             role="progressbar"
-            className='active:scale-90 transition-transform duration-100 select-none md:hover:bg-primary hover:bg-primary relative size-32 p-8 font-bold text-8xl border-4 h-52 w-40 radial-progress before:bg-transparent bg-primary text-primary-content border-primary'
+            className='active:scale-90 transition-transform duration-100 select-none md:hover:bg-primary hover:bg-primary relative size-32 p-8 font-bold md:text-8xl text-5xl border-4 md:h-52 h-40 w-28 md:w-40 radial-progress before:bg-transparent bg-primary text-primary-content border-primary'
             onClick={HandleTasbih}>
             {count}
 
           </button>
-          <div className="rounded-box absolute lg:top-55 lg:right-35 top-55 right-25 size-14 text-lg badge badge-soft badge-warning">
+          <div className="rounded-box absolute lg:top-55 lg:right-35 -top-2 right-25 size-14 text-lg badge badge-soft badge-warning">
             {progress}
           </div>
 
